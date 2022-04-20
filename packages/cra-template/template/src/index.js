47f888e883c054d21abbafb5221152e2324f2a6a
Eleven <master@eleven.net.cn>
2022-04-20 16:57:15 +0800

    perf: 优化 polyfill 注释
    affects: @eleven.fe/cra-template-typescript, @eleven.fe/cra-template

--- packages/cra-template/template/src/index.js
+++ packages/cra-template/template/src/index.js
@@ -1,13 +1,7 @@
-/**
- * import 'core-js/stable';
- * import 'regenerator-runtime/runtime';
- * 等价于 ☞ import 'react-app-polyfill/stable';
- * CRA 官方不常升级 core-js，因此，自行引入 core-js，效果一致。
- * 如果不需要考虑兼容性，可删除。
- */
+// https://github.com/zloirock/core-js#babelpolyfill
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-// 兼容支持到 IE9，如果不需要可删除。
+// https://github.com/facebook/create-react-app/blob/main/packages/react-app-polyfill/ie9.js
 import 'react-app-polyfill/ie9';
 import './assets/styles/index.less';
 import React from 'react';
